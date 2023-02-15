e444acf19dfa91304060ea652fed7eed9426f991
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-02-15 18:37:55 -0300

    remove uneccessary imports and files

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main >
         <p className="text-3xl font-bold underline">haha</p>
       </main>
     </>
