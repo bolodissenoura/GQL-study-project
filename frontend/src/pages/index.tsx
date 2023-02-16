0d8107ae4dc442976ef5bb79bbb9cfc2561120fc
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-02-15 23:53:39 -0300

    chore: header main page bname

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Dortmed - Instrumentador</title>
+        <title>Instrumentadores</title>
         <meta name="description" content="Planejamento de cirurgias para instrumentador" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
