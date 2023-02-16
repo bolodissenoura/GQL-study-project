2a32c5e7561d65fe51499ee3fd747e0e84af9bf7
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-02-16 00:14:59 -0300

    feat: modal styles preset

--- frontend/src/components/Modal/editModal.component.tsx
+++ frontend/src/components/Modal/editModal.component.tsx
@@ -26,6 +26,8 @@ export const EditModal = React.forwardRef((props: EditModalInterface, ref) => {
       right: 'auto',
       bottom: 'auto',
       marginRight: '-50%',
+      backgroundColor: "transparent",
+      border: "none",
       transform: 'translate(-50%, -50%)',
     },
   };
@@ -44,16 +46,29 @@ export const EditModal = React.forwardRef((props: EditModalInterface, ref) => {
           style={customStyles}
           contentLabel="Example Modal"
         >
-          <h2 >Hello</h2>
-          <button onClick={closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
+          <div className="relative w-full h-full max-w-2xl md:h-auto ">
+            <div className="relative rounded-lg shadow bg-gray-700">
+              <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  {props.titleModal ?? "Modal de ação"}
+                </h3>
+                <button onClick={() => closeModal()} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+                  <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+              </div>
+              <div className="p-6 space-y-6">
+
+              </div>
+              <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button data-modal-hide="defaultModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Salvar
+                </button>
+                <button onClick={() => closeModal()} data-modal-hide="defaultModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </div>
         </Modal>
       </div>
     </>
