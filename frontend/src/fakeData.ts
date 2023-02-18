e87e5b282733917d571442b0d133ad01ba8a268e
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-02-18 10:39:45 -0300

    feat: mock data with provisory file

---
+++ frontend/src/fakeData.ts
@@ -0,0 +1,8 @@
+import { OptionsInterface } from "./interfaces";
+
+export const fakeData: OptionsInterface[] = [
+  { id: "xaxsxs", label: "Daniel" },
+  { id: "xasasdxsxs", label: "Rafaella" },
+  { id: "xaxaasxs", label: "marcao" },
+  { id: "xaddsxsxs", label: " Fabi" },
+];
