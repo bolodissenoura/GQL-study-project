73fbb68b401dc65cb29edf730662cf71a6b39a8e
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-03-04 19:20:51 -0300

    feat: translate modal text

--- frontend/src/components/Modal/deleteModal.component.tsx
+++ frontend/src/components/Modal/deleteModal.component.tsx
@@ -54,7 +54,7 @@ export function DeleteModal(props: DeleteModalInterface) {
                   clip-rule="evenodd"></path>
               </svg>
               <p className="mb-4 text-gray-500 dark:text-gray-300">
-                Tem certeza que deseja deletar esse item ?
+               Sure that want to delete this surgery ?
               </p>
               <div className="flex justify-center items-center space-x-4">
                 <button
@@ -62,13 +62,13 @@ export function DeleteModal(props: DeleteModalInterface) {
                   type="button"
                   onClick={() => props.closeModalDelete()}
                   className="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                  Não, cancelar
+                  No, cancel
                 </button>
                 <button
                   type="submit"
                   onClick={() => props.confirmModalDelete()}
                   className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
-                  Sim, tenho certeza
+                  Yep
                 </button>
               </div>
             </div>
