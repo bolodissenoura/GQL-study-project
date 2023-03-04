ca4af14c54e83ee86e7f206e868acab0b8bef1b9
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-03-04 15:30:10 -0300

    feat: make the application responsive to mobile

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-400 h-auto p-5 min-h-screen overflow-x-auto">
+      <main className="bg-gray-400 h-auto p-5 min-h-screen  overflow-x-auto">
         <section className="container px-4 mx-auto">
           <div className="sm:flex sm:items-center sm:justify-between">
             <div className="flex items-center mt-4 gap-x-3">
@@ -79,14 +79,17 @@ export default function Home() {
           </div>
           <div className="mt-6 md:flex md:items-center md:justify-between">
             <div className="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
-              <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
+              <button
+                disabled
+                className="px-5 py-2 text-xs disabled:opacity-50 font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
                 View all tags
               </button>
 
               {fakeDataTags.map((item: TagsInterface, index: number) => (
                 <button
+                  disabled
                   key={index}
-                  className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+                  className="disabled:opacity-50 px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
                   <div
                     className={`inline px-3 py-1 text-sm font-normal rounded-full gap-x-2 bg-emerald-100/60 dark:bg-gray-800 text-${item.color}-500`}>
                     {item.label}
@@ -94,8 +97,7 @@ export default function Home() {
                 </button>
               ))}
             </div>
-
-            <div className="relative flex items-center mt-4 md:mt-0">
+            <div className="relative flex items-center mt-10 md:mt-0">
               <span className="absolute">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -114,18 +116,18 @@ export default function Home() {
               <input
                 type="text"
                 placeholder="Search"
-                className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80  placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
           </div>
 
           {data?.Surgeries ? (
             <>
-              <div className="flex flex-col mt-6  ">
-                <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8 ">
-                  <div className="inline-block min-w-full max-h-full py-2 align-middle md:px-6 lg:px-8">
-                    <div className="border border-gray-200 dark:border-gray-700 md:rounded-lg overflow-y-auto ">
-                      <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700 rtl:pr-11 rtl:pl-5 sm:px-6 lg:px-8">
+              <div className="flex flex-col mt-6">
+                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                    <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                      <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                         <C.TableHeader />
                         <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900 ">
                           {data?.Surgeries?.map(
