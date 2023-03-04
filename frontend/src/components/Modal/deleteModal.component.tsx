11eec749377d312fd579232084c6289a1ad948df
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-03-04 02:04:43 -0300

    fix: warning react-modal lib

--- frontend/src/components/Modal/deleteModal.component.tsx
+++ frontend/src/components/Modal/deleteModal.component.tsx
@@ -14,6 +14,7 @@ export function DeleteModal(props: DeleteModalInterface) {
     <>
       <Modal
         isOpen={props.modalState}
+        ariaHideApp={false}
         onRequestClose={props.closeModalDelete}
         style={customStyles}
         contentLabel="Delete Modal">
