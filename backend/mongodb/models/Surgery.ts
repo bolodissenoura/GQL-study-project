59ebcd61d30a5e02455bc98ae16398b77d3f2ad6
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-03-15 19:16:45 -0300

    fix: remove the User logic from Surgery models

--- backend/mongodb/models/Surgery.ts
+++ backend/mongodb/models/Surgery.ts
@@ -1,12 +1,5 @@
 import mongoose from "mongoose";
 
-interface User extends Document {
-  [x: string]: any;
-  email: string;
-  password: string;
-  name: string;
-}
-
 const schema = new mongoose.Schema({
   patient: String,
   doctor: String,
@@ -17,14 +10,5 @@ const schema = new mongoose.Schema({
   hospitalName: String,
   typeTag: String,
 });
-
-const userSchema = new mongoose.Schema<User>({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  token: { type: String, required: true },
-});
-
-export const UserModel = mongoose.model<User>("User", userSchema);
 
 export const SurgeryMongo = mongoose.model("Surgery", schema);
