11ab728d46c1fe5b87504b188d29368bf5e7ec20
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-03-20 18:47:09 -0300

    feat: toastify container to handle the component/css

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { client } from "@/lib/apollo";
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <AuthProvider>
           <Component {...pageProps} />
+          <ToastContainer />
         </AuthProvider>
       </ApolloProvider>
     </>
