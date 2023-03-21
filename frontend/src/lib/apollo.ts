e2a2aa05e9b1475ff70a1d204e2e86f4decc684b
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-03-21 15:10:33 -0300

    feat: headers auth from every query using  apollo client

--- frontend/src/lib/apollo.ts
+++ frontend/src/lib/apollo.ts
@@ -1,7 +1,18 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import fetch from "cross-fetch";
+import { parseCookies } from "nookies";
+
+const { "token-surgery-plans": token }: any = parseCookies();
+
+console.log(token);
 
 export const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:4000", fetch }),
+  link: new HttpLink({
+    uri: "http://localhost:4000",
+    fetch,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }),
   cache: new InMemoryCache(),
 });
