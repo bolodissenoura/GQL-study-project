280dbff3accad64dfc279e11d46f532024cf2c95
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-03-23 18:26:25 -0300

    disable lint rules

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,13 @@
 module.exports = {
-    extends: ["next", "turbo", "prettier"],
-    rules: {
-      "@next/next/no-html-link-for-pages": "off",
+  extends: ["next", "turbo", "prettier"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
     },
-    parserOptions: {
-      babelOptions: {
-        presets: [require.resolve("next/babel")],
-      },
-    },
-  };
+  },
+};
