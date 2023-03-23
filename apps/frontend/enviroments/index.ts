49b361d0942967976daf9958299c7b0f0ada31e2
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-03-23 00:40:11 -0300

    hotfix: enviroments var

---
+++ apps/frontend/enviroments/index.ts
@@ -0,0 +1,4 @@
+const enviroments = {
+  BASE_URL: process.env.NEXT_PUBLIC_REACT_APP_BASE_URL,
+};
+export default enviroments;
--- apps/frontend/src/lib/apollo.ts
+++ apps/frontend/src/lib/apollo.ts
@@ -1,12 +1,13 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import fetch from "cross-fetch";
 import { parseCookies } from "nookies";
+import enviroment from "../../enviroments";
 
 const { "token-surgery-plans": token }: any = parseCookies();
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000",
+    uri: enviroment.BASE_URL,
     fetch,
     headers: {
       Authorization: `Bearer ${token}`,
