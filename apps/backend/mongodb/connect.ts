4c98f4a26663c644060390b2c077f4385d4641e0
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-03-23 17:30:48 -0300

    hotfix: mongoose strictQuery

--- apps/backend/mongodb/connect.ts
+++ apps/backend/mongodb/connect.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose
+  .set("strictQuery", false)
   .connect(process.env.MONGODB_API as string)
   .then(() => console.log("Mongodb connected"))
   .catch((error) => console.log(error));
